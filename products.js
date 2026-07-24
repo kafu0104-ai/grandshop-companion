c465312aae763594796ce2c868b3d47ddab1b29c
kafu0104-ai <kafu0104@gmail.com>
2026-07-24 20:18:11 +0900

    Update products.js

--- products.js
+++ products.js
@@ -3039,7 +3039,7 @@ const PRODUCTS_RAW = [
     "limit": 5,
     "category": "生活雑貨",
     "sales": "イベント・通販限定",
-    "image": "https://www.utapri.com/grandshop/archive/2026/assets/img/goods/m016_rz3g/b01.jpg",
+    "image": "https://www.utapri.com/grandshop/archive/2026/assets/img/goods/b026_u79d/thumb01.jpg",
     "detail": "https://www.utapri.com/grandshop/item.php?id=b026",
     "random": false
   },
